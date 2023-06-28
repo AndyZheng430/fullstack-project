7e1f07ff106eb8808ad41ae721c86db904c3b64a
AndyZheng430 <andy.zheng.430@stonybrook.edu>
2023-06-28 17:31:51 -0400

    Added backend endpoint /order to process post request from the form

--- intsureview_be/intsureview_be/apps/api/serializers.py
+++ intsureview_be/intsureview_be/apps/api/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User, Group
+from django.forms import ModelForm;
 from rest_framework import serializers
+from intsureview_be.apps.api.models import RideModel;
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +14,12 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
+
+
+class RideForm(ModelForm):
+    class Meta:
+        model = RideModel
+        fields = ["name", "start", "end", "ride_type", "add_donuts", "comments"]
+
+    def clean(self):
+        super(RideForm, self).clean()
--- intsureview_be/intsureview_be/apps/api/views.py
+++ intsureview_be/intsureview_be/apps/api/views.py
@@ -1,7 +1,13 @@
+import json
 from django.contrib.auth.models import User, Group
+from django.http import QueryDict
+from django.shortcuts import HttpResponse
 from rest_framework import viewsets
 from rest_framework import permissions
-from intsureview_be.apps.api.serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from intsureview_be.apps.api.serializers import UserSerializer, GroupSerializer, RideForm
+from intsureview_be.apps.api.models import RideModel;
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,3 +28,27 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+def RidePostView(request):
+    if request.method == "POST":
+        request_body_json = json.loads(request.body.decode())
+        print(request_body_json)
+
+        form_data = QueryDict("", mutable=True)
+
+        for key, value in request_body_json.items():
+            form_data.update({key: value})
+
+        details = RideForm(form_data)
+
+        if details.is_valid():
+            return HttpResponse("Data is valid")
+        else:
+            response = HttpResponse("Form is invalid")
+            response.status_code = 400
+            return response
+    else:
+        response = HttpResponse("Method Not Allowed")
+        response.status_code = 405
+        return response
